7d26c5e7606cc3fa70ab99e87a30054d1bb61ae0
Tomoki Aonuma <uasi@uasi.jp>
2015-05-19 20:21:30 +0900

    Print formatted JSON by default

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,46 @@ extern crate serde;
 
 use clap::{App, Arg};
 use std::collections::btree_map::BTreeMap;
+use std::fs::File;
+use std::io::Read;
 use serde::json;
 
 mod parse;
 
+fn slurp(path: &str) -> Option<String> {
+    File::open(path).ok().as_mut().and_then(|f| {
+        let mut s = String::new();
+        f.read_to_string(&mut s).ok().and_then(|_| Some(s))
+    })
+}
+
 impl parse::Value {
     fn to_json(&self) -> json::Value {
+        let maybe_s = match *self {
+            parse::Value::DataFile(ref path)    => slurp(path),
+            parse::Value::DataString(ref s)     => Some(s.to_string()),
+            parse::Value::FormFile(ref path)    => slurp(path),
+            parse::Value::HttpHeader(ref s)     => Some(s.to_string()),
+            parse::Value::RawJsonString(ref s)  => Some(s.to_string()),
+            parse::Value::RawJsonFile(ref path) => slurp(path).and_then(|s| json::from_str(&s).ok()),
+            parse::Value::UrlParam(ref s)       => Some(s.to_string()),
+        };
+        match maybe_s {
+            Some(s) => json::Value::String(s),
+            None    => json::Value::Null,
+        }
+    }
+
+    fn to_typed_json(&self) -> json::Value {
         let mut map = BTreeMap::new();
         let (ty, value) = match *self {
-            parse::Value::DataFile(ref s)      => ("data_file", s),
-            parse::Value::DataString(ref s)    => ("data_string", s),
-            parse::Value::FormFile(ref s)      => ("form_file", s),
-            parse::Value::HttpHeader(ref s)    => ("http_header", s),
-            parse::Value::RawJsonString(ref s) => ("raw_json_string", s),
-            parse::Value::RawJsonFile(ref s)   => ("raw_json_file", s),
-            parse::Value::UrlParam(ref s)      => ("url_param", s),
+            parse::Value::DataFile(ref path)    => ("data_file", path),
+            parse::Value::DataString(ref s)     => ("data_string", s),
+            parse::Value::FormFile(ref path)    => ("form_file", path),
+            parse::Value::HttpHeader(ref s)     => ("http_header", s),
+            parse::Value::RawJsonString(ref s)  => ("raw_json_string", s),
+            parse::Value::RawJsonFile(ref path) => ("raw_json_file", path),
+            parse::Value::UrlParam(ref s)       => ("url_param", s),
         };
         map.insert("type".to_string(), json::Value::String(ty.to_string()));
         map.insert("value".to_string(), json::Value::String(value.to_string()));
@@ -25,10 +50,21 @@ impl parse::Value {
     }
 }
 
+fn to_json(kvs: &[(String, Option<parse::Value>)]) -> String {
+    let mut map = BTreeMap::new();
+    for kv in kvs.iter() {
+        let k = kv.0.clone();
+        let v = kv.1.clone().map(|v| v.to_json()).unwrap_or(json::Value::Null);
+        map.insert(k, v);
+    }
+    let root = json::Value::Object(map);
+    json::ser::to_string(&root).unwrap_or("".to_string())
+}
+
 fn dump(kvs: &[(String, Option<parse::Value>)]) -> String {
     let kv_arrays = kvs.iter().map(|kv| {
         let k = json::Value::String(kv.0.clone());
-        let v = kv.1.clone().map(|v| v.to_json()).unwrap_or(json::Value::Null);
+        let v = kv.1.clone().map(|v| v.to_typed_json()).unwrap_or(json::Value::Null);
         json::Value::Array(vec![k, v])
     }).collect();
     let root = json::Value::Array(kv_arrays);
@@ -38,11 +74,19 @@ fn dump(kvs: &[(String, Option<parse::Value>)]) -> String {
 fn main() {
     let matches = App::new("skv")
         .version("0.1.0")
+        .arg(Arg::with_name("dump")
+            .long("dump")
+            .takes_value(false)
+            .required(false))
         .arg(Arg::with_name("key-value pairs")
             .index(1)
             .multiple(true))
         .get_matches();
-    let pairs = matches.values_of("key-value pairs").unwrap_or(vec![]);
-    let parsed = parse::parse(&pairs);
-    println!("dump => {}", dump(&parsed));
+    let items = matches.values_of("key-value pairs").unwrap_or(vec![]);
+    let kvs = parse::parse(&items);
+    if matches.is_present("dump") {
+        println!("{}", dump(&kvs));
+    } else {
+        println!("{}", to_json(&kvs));
+    }
 }
