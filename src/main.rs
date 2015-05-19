78b0e6a8f311da3ee0538b6f615758b056a9d211
Tomoki Aonuma <uasi@uasi.jp>
2015-05-19 12:16:45 +0900

    Parse arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate clap;
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("skv")
+        .version("0.1.0")
+        .arg(Arg::with_name("key-value pairs")
+            .index(1)
+            .multiple(true))
+        .get_matches();
+    let pairs = matches.values_of("key-value pairs").unwrap_or(vec![]);
+    println!("pairs: {:?}", pairs);
 }
