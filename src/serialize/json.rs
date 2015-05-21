e584f2baf43841a635618b73168b517a8ae74ded
Tomoki Aonuma <uasi@uasi.jp>
2015-05-21 12:03:41 +0900

    slurp() returns io::Result

--- src/serialize/json.rs
+++ src/serialize/json.rs
@@ -2,17 +2,17 @@ use parse;
 use serde::json;
 use std::collections::btree_map::BTreeMap;
 use std::fs::File;
-use std::io::Read;
+use std::io::{self, Read};
 
 impl parse::Value {
     fn to_json(&self) -> json::Value {
         let maybe_s = match *self {
-            parse::Value::DataFile(ref path)    => slurp(path),
+            parse::Value::DataFile(ref path)    => slurp(path).ok(),
             parse::Value::DataString(ref s)     => Some(s.to_string()),
-            parse::Value::FormFile(ref path)    => slurp(path),
+            parse::Value::FormFile(ref path)    => slurp(path).ok(),
             parse::Value::HttpHeader(ref s)     => Some(s.to_string()),
             parse::Value::RawJsonString(ref s)  => Some(s.to_string()),
-            parse::Value::RawJsonFile(ref path) => slurp(path).and_then(|s| json::from_str(&s).ok()),
+            parse::Value::RawJsonFile(ref path) => slurp(path).ok().and_then(|s| json::from_str(&s).ok()),
             parse::Value::UrlParam(ref s)       => Some(s.to_string()),
         };
         match maybe_s {
@@ -33,9 +33,9 @@ pub fn to_json(pairs: &[parse::Pair]) -> String {
     json::ser::to_string(&root).unwrap_or("".to_string())
 }
 
-fn slurp(path: &str) -> Option<String> {
-    File::open(path).ok().as_mut().and_then(|f| {
-        let mut s = String::new();
-        f.read_to_string(&mut s).ok().and_then(|_| Some(s))
-    })
+fn slurp(path: &str) -> io::Result<String> {
+    let mut f = try!(File::open(path));
+    let mut s = String::new();
+    try!(f.read_to_string(&mut s));
+    Ok(s)
 }
